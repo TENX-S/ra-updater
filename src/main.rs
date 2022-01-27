5c4d164b3e055d69148c45760343b31d67dbec86
TENX-S <ttenx@pm.me>
2022-01-27 13:58:32 +0800

    refactor: release 1.0.0
    * remove config file
    * add dev profile
    * add .cargo for ASAN on unix

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,21 @@
 // #![feature(write_all_vectored)]
 use anyhow::Result;
 use bytes::Buf;
-use clap::{Parser, Subcommand};
+use clap::{ArgEnum, Parser, Subcommand};
 use flate2::bufread::GzDecoder;
 // use rayon::prelude::*;
 use reqwest::header::{HeaderValue, CONTENT_LENGTH, RANGE};
 use reqwest::StatusCode;
-use serde::{Deserialize, Serialize};
 use serde_json::Value;
 use std::env;
 use std::fs::{self, File, OpenOptions};
-use std::io::prelude::*;
-use std::io::{self, BufReader, BufWriter, Seek, SeekFrom};
+use std::io::{self, prelude::*, BufReader, BufWriter, Seek, SeekFrom};
 use std::path::{Path, PathBuf};
 use std::process::Command;
 use std::str::FromStr;
 use std::sync::Arc;
-use tokio::time::Instant;
+use std::time::Instant;
+use strum_macros::Display;
 
 const RA_DL_BASE: &str = "https://github.com/rust-analyzer/rust-analyzer/releases/";
 const RA_REL_API_BASE: &str = "https://api.github.com/repos/rust-analyzer/rust-analyzer/releases/";
@@ -24,25 +23,10 @@ const MIRROR: &str = "https://github.91chi.fun//";
 const PAR_DL_BUF_SIZE: u64 = 512 * 1024;
 static APP_USER_AGENT: &str = concat!(env!("CARGO_PKG_NAME"), "/", env!("CARGO_PKG_VERSION"),);
 
-#[derive(Debug)]
-struct RaUpdater {
-    cli: Cli,
-    ver: RaVersion,
-}
-
-impl RaUpdater {
-    fn new() -> Result<Self> {
-        Ok(Self {
-            cli: Cli::parse(),
-            ver: RaVersion::load_from_file()?,
-        })
-    }
-}
-
 #[derive(Parser, Debug)]
 #[clap(author, version, about)]
 struct Cli {
-    /// Accelerate downloads for users in China
+    /// F** the [GFW](https://wikipedia.org/wiki/Great_Firewall)
     #[clap(short = 'a', long, conflicts_with = "check")]
     mirror: bool,
 
@@ -54,12 +38,85 @@ struct Cli {
     #[clap(short, long, conflicts_with = "check")]
     mt: bool,
 
-    /// Force to update rust-analyzer
+    /// Force to update rust-analyzer in current release channel
     #[clap(short, long, conflicts_with = "check")]
     force: bool,
 
     #[clap(subcommand)]
     cmd: Option<Cmd>,
+}
+
+#[derive(Debug, Subcommand)]
+enum Cmd {
+    /// Set release channel for rust-analyzer
+    Channel {
+        /// stable or nightly
+        #[clap(arg_enum)]
+        rel_chan: ReleaseChannel,
+
+        /// F** the [GFW](https://wikipedia.org/wiki/Great_Firewall)
+        #[clap(short = 'a', long, conflicts_with = "check")]
+        mirror: bool,
+
+        /// Download rust-analyzer in multi-threaded way
+        #[clap(short, long, conflicts_with = "check")]
+        mt: bool,
+    },
+}
+
+#[derive(Display, Debug, ArgEnum, Clone, Copy, PartialEq)]
+#[strum(serialize_all = "snake_case")]
+enum ReleaseChannel {
+    Stable,
+    Nightly,
+}
+
+#[derive(Debug)]
+struct RaVersion {
+    commitish: String,
+    channel: ReleaseChannel,
+}
+
+impl RaVersion {
+    fn parse(cli: &Cli) -> Result<Self> {
+        if !ra_exec_path().exists() {
+            println!("rust-analyzer not found. Downloading ...");
+            if let Some(Cmd::Channel { rel_chan: value, mirror, mt }) = cli.cmd {
+                let dl_url = ra_remote(value, mirror)?.1;
+                ra_update(&dl_url, mt)?;
+                std::process::exit(0);
+            } else {
+                let dl_url = ra_remote(ReleaseChannel::Stable, cli.mirror)?.1;
+                ra_update(&dl_url, cli.mt)?;
+                std::process::exit(0);
+            }
+        } else {
+            let version = ra_version()?;
+            let segments: Vec<&str> = version.split(' ').collect();
+            let channel = if segments[3].trim() == "stable" {
+                ReleaseChannel::Stable
+            } else {
+                ReleaseChannel::Nightly
+            };
+            Ok(Self {
+                commitish: segments[1].trim().into(),
+                channel,
+            })
+        }
+    }
+
+    fn set_channel(&mut self, channel: ReleaseChannel, mirror: bool, mt: bool) -> Result<()> {
+        if self.channel != channel {
+            println!("Switching to {:?} channel ...", channel);
+            let dl_url = ra_remote(channel, mirror)?.1;
+            ra_update(&dl_url, mt)?;
+            println!("Done");
+            Ok(())
+        } else {
+            println!("You are already in stable channel");
+            Ok(())
+        }
+    }
 }
 
 #[derive(Debug)]
@@ -91,102 +148,15 @@ impl Iterator for PartialRangeIter {
     }
 }
 
-#[derive(Debug, Subcommand)]
-enum Cmd {
-    /// Set update channel for rust-analyer
-    Channel {
-        /// stable or nightly
-        value: String,
-
-        /// Accelerate downloads for users in China
-        #[clap(short = 'a', long, conflicts_with = "check")]
-        mirror: bool,
-
-        /// Download rust-analyzer in multi-threaded way
-        #[clap(short, long, conflicts_with = "check")]
-        mt: bool,
-    },
-}
-
-#[derive(Debug, Deserialize, Serialize)]
-struct RaVersion {
-    commitish: String,
-    channel: String,
-}
-
-impl RaVersion {
-    fn load_from_file() -> Result<Self> {
-        let cfg_path = PathBuf::from(ra_home()).join("config.toml");
-        if !cfg_path.exists() {
-            let version = Self::load_from_exec();
-            version.save()?;
-            Ok(version)
-        } else {
-            let mut reader = BufReader::new(File::open(cfg_path)?);
-            let mut content = String::new();
-            reader.read_to_string(&mut content)?;
-            Ok(toml::from_str(&content)?)
-        }
-    }
-
-    fn load_from_exec() -> Self {
-        let ra_output = Command::new(ra_path())
-            .current_dir(ra_home())
-            .arg("--version")
-            .output()
-            .expect("Failed to exec rust-analyzer!");
-        let version =
-            String::from_utf8(ra_output.stdout).expect("Failed to get output of rust-analyzer");
-        let segments: Vec<&str> = version.split(' ').collect();
-        Self {
-            commitish: segments[1].trim().into(),
-            channel: segments[3].trim().into(),
-        }
-    }
-
-    fn set_channel(&mut self, channel: &str, mirror: bool, mt: bool) -> Result<()> {
-        if self.channel != channel {
-            println!("Switching to {} channel ...", channel);
-            let dl_url = ra_remote(channel, mirror)?.1;
-            ra_update(&dl_url, mt)?;
-            *self = Self::load_from_exec();
-            self.save()?
-        }
-        println!("Done");
-        Ok(())
-    }
-
-    fn save(&self) -> Result<()> {
-        let cfg_path = PathBuf::from(ra_home()).join("config.toml");
-        let file = OpenOptions::new()
-            .read(true)
-            .write(true)
-            .create(true)
-            .open(cfg_path)?;
-        let mut writer = BufWriter::new(file);
-        let content = toml::to_string(self)?;
-        writer.write_all(content.as_bytes())?;
-        Ok(())
-    }
-}
-
 fn main() -> Result<()> {
-    let ra_updater = RaUpdater::new()?;
-    let cli = ra_updater.cli;
-    let mut ver = ra_updater.ver;
+    let cli = Cli::parse();
+    let mut ver = RaVersion::parse(&cli)?;
 
-    let (api, dl_url) = &ra_remote(&ver.channel, cli.mirror)?;
+    let (rel_api, dl_url) = &ra_remote(ver.channel, cli.mirror)?;
     match cli.cmd {
-        Some(Cmd::Channel {
-            ref value,
-            mirror,
-            mt,
-        }) => match value.as_ref() {
-            "stable" | "nightly" => ver.set_channel(value, mirror, mt)?,
-            _ => {}
-        },
+        Some(Cmd::Channel { rel_chan: value, mirror, mt }) => ver.set_channel(value, mirror, mt)?,
         None => {
-            let up_to_date = check_update(api, &ver.commitish)?;
+            let up_to_date = check_update(rel_api, &ver.commitish)?;
             if cli.check {
                 if up_to_date {
                     println!("Already up-to-date")
@@ -208,26 +178,11 @@ fn main() -> Result<()> {
     Ok(())
 }
 
-fn ra_remote(channel: &str, mirror: bool) -> Result<(String, String)> {
-    let mut api_tag = "latest";
-    let mut dl_tag = "latest/download/";
-    if channel == "nightly" {
-        api_tag = "tags/nightly";
-        dl_tag = "download/nightly/";
-    }
-    let api_url = format!("{}{}", RA_REL_API_BASE, api_tag);
-    let mut dl_url = format!("{}{}{}", RA_DL_BASE, dl_tag, ra_name());
-    if mirror {
-        dl_url.insert_str(0, MIRROR);
-    }
-    Ok((api_url, dl_url))
-}
-
-fn check_update(api: &str, curr: &str) -> Result<bool> {
+fn check_update(rel_api: &str, curr: &str) -> Result<bool> {
     let client = reqwest::blocking::Client::builder()
         .user_agent(APP_USER_AGENT)
         .build()?;
-    let resp = client.get(api).send()?.text()?;
+    let resp = client.get(rel_api).send()?.text()?;
     let body: Value = serde_json::from_str(&resp)?;
     let latest = body["target_commitish"]
         .as_str()
@@ -235,16 +190,16 @@ fn check_update(api: &str, curr: &str) -> Result<bool> {
     Ok(latest.starts_with(curr))
 }
 
-fn ra_update(url: &str, mt: bool) -> Result<()> {
-    let tmp_path = dirs_next::download_dir()
-        .unwrap()
-        .join("rust-analyzer_ra_updater.gz");
-    let mut writer = BufWriter::new(
+fn ra_update(dl_url: &str, mt: bool) -> Result<()> {
+    let temp = dirs_next::download_dir()
+        .unwrap() // Never panic, ra_name() guarantees it
+        .join("rust-analyzer_ra_updater_temp.gz");
+    let mut dl_writer = BufWriter::new(
         OpenOptions::new()
             .create(true)
             .write(true)
             .truncate(true)
-            .open(&tmp_path)?,
+            .open(&temp)?,
     );
     let now = Instant::now();
     if mt {
@@ -255,34 +210,37 @@ fn ra_update(url: &str, mt: bool) -> Result<()> {
         let rt = Runtime::new()?;
         rt.block_on(async {
             let client = Arc::new(reqwest::Client::new());
-            let resp_header = client.head(url).send().await?;
+            let resp_header = client.head(dl_url).send().await?;
             let content_length = resp_header
                 .headers()
                 .get(CONTENT_LENGTH)
                 .expect("response doesn't include the content length");
             let size = u64::from_str(content_length.to_str()?)
                 .expect("Error: Invalid Content-Length header");
-            let mut dl = vec![];
+            let mut chunks_cnt = size / PAR_DL_BUF_SIZE + 1;
+            let mut dl = Vec::with_capacity(chunks_cnt as usize);
             let (tx, mut rx) = unbounded_channel();
             let tx = Arc::new(tx);
             PartialRangeIter::new(0, size - 1).for_each(|(range, start)| {
-                let url = url.to_owned();
+                let url = dl_url.to_owned();
                 let client = client.clone();
                 let txc = tx.clone();
                 dl.push(tokio::spawn(async move {
                     let par_resp = client.get(url).header(RANGE, range).send().await?;
                     let status = par_resp.status();
                     if !(status == StatusCode::OK || status == StatusCode::PARTIAL_CONTENT) {
-                        panic!("Error: Unexpected server response: {}", status);
+                        anyhow::bail!("Error: Unexpected server response: {}", status);
                     }
                     let par_bytes = par_resp.bytes().await?;
                     txc.send((par_bytes, start))?;
                     Ok::<(), anyhow::Error>(())
                 }));
             });
-            println!("spawn time: {}", now.elapsed().as_secs_f64());
-            let mut chunks_cnt = size / PAR_DL_BUF_SIZE + 1;
-            println!("cnt: {}", chunks_cnt);
+            println!(
+                "Spawn {} tasks: {:.2}s",
+                chunks_cnt,
+                now.elapsed().as_secs_f64()
+            );
 
             tokio::task::block_in_place(move || {
                 let (tx, _rx) = unbounded();
@@ -293,12 +251,12 @@ fn ra_update(url: &str, mt: bool) -> Result<()> {
                             chunks_cnt -= 1;
                         }
                     }
-                    println!("Download: {}", now.elapsed().as_secs_f64());
+                    println!("Download: {:.2}s", now.elapsed().as_secs_f64());
                     Ok::<(), anyhow::Error>(())
                 })?;
                 while let Ok((par_bytes, start)) = _rx.recv() {
-                    writer.seek(SeekFrom::Start(start))?;
-                    writer.write_all(par_bytes.as_ref())?;
+                    dl_writer.seek(SeekFrom::Start(start))?;
+                    dl_writer.write_all(par_bytes.as_ref())?;
                 }
                 // use std::io::IoSlice;
                 // let mut buf = vec![];
@@ -311,19 +269,19 @@ fn ra_update(url: &str, mt: bool) -> Result<()> {
 
                 Ok::<(), anyhow::Error>(())
             })?;
-            ra_extract(&tmp_path)?;
-            fs::remove_file(tmp_path)?;
+            ra_extract(&temp)?;
+            fs::remove_file(temp)?;
             Ok::<(), anyhow::Error>(())
         })?;
 
         return Ok(());
     }
 
-    let mut bytes_reader = reqwest::blocking::get(url)?.bytes()?.reader();
-    println!("Download: {}", now.elapsed().as_secs_f64());
-    io::copy(&mut bytes_reader, &mut writer).unwrap();
-    ra_extract(&tmp_path)?;
-    fs::remove_file(tmp_path)?;
+    let mut bytes_reader = reqwest::blocking::get(dl_url)?.bytes()?.reader();
+    println!("Download: {:.2}s", now.elapsed().as_secs_f64());
+    io::copy(&mut bytes_reader, &mut dl_writer)?;
+    ra_extract(&temp)?;
+    fs::remove_file(temp)?;
 
     Ok(())
 }
@@ -336,18 +294,49 @@ fn ra_extract(path: impl AsRef<Path>) -> Result<()> {
             .create(true)
             .write(true)
             .truncate(true)
-            .open(ra_path())
-            .expect("Error: rust-analyzer is being used!"),
+            .open(ra_exec_path())?,
     );
     io::copy(&mut gz, &mut target)?;
     Ok(())
 }
 
+fn ra_version() -> Result<String> {
+    let ra_output = Command::new(ra_exec_path())
+        .current_dir(ra_home())
+        .arg("--version")
+        .output()?;
+    Ok(String::from_utf8(ra_output.stdout)?)
+}
+
 #[inline]
-fn ra_path() -> PathBuf {
+fn ra_remote(channel: ReleaseChannel, mirror: bool) -> Result<(String, String)> {
+    let (api_tag, dl_tag) = match channel {
+        ReleaseChannel::Stable => ("latest", "latest/download/"),
+        ReleaseChannel::Nightly => ("tags/nightly", "download/nightly/"),
+    };
+    let rel_api = format!("{}{}", RA_REL_API_BASE, api_tag);
+    let mut dl_url = format!("{}{}{}", RA_DL_BASE, dl_tag, ra_name());
+    if mirror {
+        dl_url.insert_str(0, MIRROR);
+    }
+    Ok((rel_api, dl_url))
+}
+
+#[inline]
+fn ra_exec_path() -> PathBuf {
     PathBuf::from(ra_home()).join(ra_exec())
 }
 
+#[inline]
+fn ra_exec() -> String {
+    let mut ra_exec = "rust-analyzer".to_owned();
+    if cfg!(target_os = "windows") {
+        ra_exec.push_str(".exe")
+    }
+    ra_exec
+}
+
+#[inline]
 fn ra_home() -> String {
     if let Ok(ra_dir) = env::var("RA_HOME") {
         if !PathBuf::from(&ra_dir).exists() {
@@ -362,6 +351,7 @@ fn ra_home() -> String {
     }
 }
 
+#[inline]
 fn ra_name() -> String {
     let arch = {
         if cfg!(target_arch = "x86_64") {
@@ -394,13 +384,4 @@ fn ra_name() -> String {
         std::process::exit(-1);
     }
     format!("rust-analyzer-{}-{}.gz", arch, distributor)
-}
-
-#[inline]
-fn ra_exec() -> String {
-    let mut ra_exec = "rust-analyzer".to_owned();
-    if cfg!(windows) {
-        ra_exec.push_str(".exe")
-    }
-    ra_exec
 }
